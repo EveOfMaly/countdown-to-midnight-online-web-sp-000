8fe5d598a395d369eb9283246af99af19a0115ba
John Kyeremeh <john.a.kyeremeh@gmail.com>
2021-05-02 01:22:55 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
-#write your code here
 
-def countdown
+
+def countdown(number)
+  while number > 1 
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+   return "HAPPY NEW YEAR!"
 end
+
+countdown(3)
+
